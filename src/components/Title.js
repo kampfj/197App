986b358562fdd06c30769112c320b70e2942b50a
JJ Kampf <kampfj@sas.upenn.edu>
2021-03-02 11:28:11 -0500

    title cleanup

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyledH1 } from './Styled.js'
 
-const Title = () => ( <StyledH1>A Very Temporary Chat With Yourself</StyledH1> )
+const Title = () => ( <StyledH1>a very temporary chat with yourself</StyledH1> )
 
 export default Title
